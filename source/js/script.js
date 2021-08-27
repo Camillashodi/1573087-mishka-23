1f3e49829643315d52b516ed52ea5778982358dc
Camillashodi <camillashodi@gmail.com>
2021-08-27 16:55:16 +0300

    Автоматизация сборки личного проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,6 @@
+const mainNav = document.querySelector(".main-nav");
+const navList = mainNav.querySelector(".main-nav__list");
+const navToggleButton = mainNav.querySelector(".main-nav__toggle");
 const modal = document.querySelector(".modal-order");
 const modalButtom = document.querySelector(".weekly-product__order");
 const catalog = document.querySelector(".catalog");
@@ -22,6 +25,15 @@ let addEventOnButton = function (button, modal) {
     }
   });
 };
+
+navList.classList.remove("main-nav__list--nojs");
+
+navToggleButton.addEventListener("click", function() {
+  navList.classList.toggle("main-nav__list--active");
+  navToggleButton.classList.toggle("main-nav__toggle--active");
+});
+
+
 
 
 if (modalButtom !== null) {
